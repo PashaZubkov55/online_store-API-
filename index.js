07b4147dd6d9e1dd5bbfe7e1bb7026797ca334c2
Pavel <pal5555@mail.ru>
2024-02-27 06:25:34 +0300

    db connect

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 require('dotenv').config()
 const express = require('express')
+const sequelize = require('./db')
+
+
 const port = process.env.PORT || 5000
+
+
 const app = express()
 
-app.listen(port,()=>{
+const start  =  async ()=>{
     try {
-        console.log('run server...',port)
-    } catch (e) {
-        console.log('error', e.message)
-        
+        await sequelize.authenticate()
+        await  sequelize.sync()
+        app.listen(port,()=> console.log('run server...',port))
+    } catch (error) {
+        console.log(error.message)
     }
-})
+}
+
+start()
