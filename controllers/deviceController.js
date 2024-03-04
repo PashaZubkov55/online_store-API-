59eeedfe45cd9cc3b43b0d0d0f4b8def771af04b
Pavel <pal5555@mail.ru>
2024-03-04 17:02:19 +0300

    pagination

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -4,22 +4,25 @@ const{Device} = require('../models/models')
 const ApiError = require('../error/ApiError')
 class DeviceController{
    async getAll  (req, res){
-      const {brandId, typeId  } = req.query 
+      let {brandId, typeId, limit, page  } = req.query
+      page = page || 1 
+      limit = limit || 9
+      let offset = page * limit - limit
       let divices;
       if (!brandId && !typeId) {
-        divices = await Device.findAll()
+        divices = await Device.findAll({limit, offset})
       }
 
       if (brandId && !typeId) {
-        divices = await Device.findAll({where: {brandId}})
+        divices = await Device.findAndCountAll({where: {brandId}, limit, offset})
       }
       if (!brandId && typeId) {
-        divices = await Device.findAll({where: {typeId}})
+        divices = await Device.findAndCountAll({where: {typeId}, limit, offset})
          
       }
      
       if (brandId && typeId) {
-        divices = await Device.findAll({where: {brandId ,typeId}})
+        divices = await Device.findAndCountAll({where: {brandId ,typeId},limit, offset})
          
       }
       return res.json(divices)
